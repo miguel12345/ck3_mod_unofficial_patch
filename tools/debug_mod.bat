c86925a54fc16c22ca0577311df901e14e04f2ec
Miguel Ferreira <amigueloferreira@gmail.com>
2023-01-25 16:19:55 +0000

    Added misc stuff

---
+++ tools/debug_mod.bat
@@ -0,0 +1,6 @@
+copy "hs_fix.mod" "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\mod\hs_fix.mod"
+copy "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json" "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json.bk"
+copy "_dlc_load.json" "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json"
+start /wait /d "C:\Program Files (x86)\Steam\steamapps\common\Crusader Kings III\binaries" ck3.exe -debug_mode -develop
+copy "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json.bk" "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json"
+DEL  "%UserProfile%\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json.bk"
